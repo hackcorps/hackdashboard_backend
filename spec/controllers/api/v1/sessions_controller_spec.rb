2e2dd0980e8d2b97ba2d19d784b42f94e0c6004e
Vasyl Varkholyak <varkholyak@gmail.com>
2015-10-01 14:22:01 +0300

    Added macros for contorllers.

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Users::SessionsController, type: :controller do
-	describe 'POST #create' do
+	context 'SIGN_IN' do
+		context 'AS ADMIN' do
+			describe 'POST #create' do
+			
+			end
+		end
 
+		context 'AS CUSTOMER' do
+			describe 'POST #create' do
+
+			end
+		end
+
+		context 'AS TEAM MEMBER' do
+			describe 'POST #create' do
+
+			end
+		end
+
+		context 'AS PROJECT MANAGER' do
+			describe 'POST #create' do
+
+			end
+		end
 	end
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,31 +2,38 @@ module ControllerMacros
 	def sign_in_as_admin
 		before(:each) do
 			@request.env["devise.mapping"] = Devise.mappings[:admin]
-			sign_in FactoryGirl.create(:admin) # Using factory girl as an example
+			@current_user = FactoryGirl.create(:admin)
+			sign_in @current_user
 		end
 	end
 
 	def sign_in_as_customer
 		before(:each) do
 			@request.env["devise.mapping"] = Devise.mappings[:customer]
-			# user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
-			sign_in FactoryGirl.create(:customer)
+			@current_user = FactoryGirl.create(:customer)
+			sign_in @current_user
 		end
 	end
 
 	def sign_in_as_team_member
 		before(:each) do
 			@request.env["devise.mapping"] = Devise.mappings[:team_member]
-			# user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
-			sign_in FactoryGirl.create(:team_member)
+			@current_user = FactoryGirl.create(:team_member)
+			sign_in @current_user
 		end
 	end
 
 	def sign_in_as_project_manager
 		before(:each) do
 			@request.env["devise.mapping"] = Devise.mappings[:project_manager]
-			# user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
-			sign_in FactoryGirl.create(:project_manager)
+			@current_user = FactoryGirl.create(:project_manager)
+			sign_in @current_user
+		end
+	end
+
+	def sign_out_user
+		before(:each) do
+			sign_out(@current_user) rescue nil
 		end
 	end
 end
