091f44dbe4452f80944806a6542e774cff3d4cae
SofiaNabyvanets <sofia.nabivanec@gmail.com>
2015-10-16 18:47:54 +0300

    Add project to user

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ActiveRecord::Base
   belongs_to :organization
   has_many :projects_users
   has_many :users, through: :projects_users
+  accepts_nested_attributes_for :projects_users, :allow_destroy => true
 end
