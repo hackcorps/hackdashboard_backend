551400cd0e6277a52760a9657d1dbf6f4fc11ecf
SofiaNabyvanets <sofia.nabivanec@gmail.com>
2015-10-19 17:56:31 +0300

    Ghange header

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
   def create
-    @user = User.find_by(invite_token: [:invite_token])
+    bind
+    @user = User.find_by(invite_token: params[:invite_token])
     if(@user)
       if @user.update(full_name: params[:full_name], password: params[:password],
           password_confirmation: params[:password_confirmation], invite_token: '' )
