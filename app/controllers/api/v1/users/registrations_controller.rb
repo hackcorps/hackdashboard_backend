5712753ae4b5e98f314cdc7a7c02741ef4946d1e
SofiaNabyvanets <sofia.nabivanec@gmail.com>
2015-10-19 17:23:43 +0300

    Change header

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
   def create
-    @user = User.find_by(invite_token: params[:user][:invite_token])
+    @user = User.find_by(invite_token: [:invite_token])
     if(@user)
-      if @user.update(full_name: params[:user][:full_name], password: params[:user][:password],
-          password_confirmation: params[:user][:password_confirmation], invite_token: '' )
+      if @user.update(full_name: params[:full_name], password: params[:password],
+          password_confirmation: params[:password_confirmation], invite_token: '' )
       render json: { status: true, user: @user }, status: 422
       else
         render json: { status: false, errors: @user.errors.full_messages }, status: 422
@@ -15,11 +15,6 @@ class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
 
   def update
     super
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:full_name, :password, :password_confirmation)
   end
 
 
