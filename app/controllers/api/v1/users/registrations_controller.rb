a1890578cbe2646bc438023c324b8d531d5bd6d2
SofiaNabyvanets <sofia.nabivanec@gmail.com>
2015-10-20 16:50:21 +0300

    Add status

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -5,9 +5,9 @@ class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
       if @user.update(full_name: params[:full_name], password: params[:password],
           password_confirmation: params[:password_confirmation], invite_token: '' )
         sign_in(@user)
-        render json: { user: { email: @user.email, role: @user.role, auth_token: AuthenticationService.new(@user).auth_token } },  status: 422
+        render json: { success: true, user: { email: @user.email, role: @user.role, auth_token: AuthenticationService.new(@user).auth_token } }
       else
-        render json: { status: false, errors: @user.errors.full_messages }, status: 422
+        render json: { success: false, errors: @user.errors.full_messages }, status: 422
       end
     else
       render json: { status: false, errors: [{ invite_token: 'Invite token not founded' }]}
